17e0133836e1db3856927314fa051f0bd144e76d
Pratik Mane <manepratik3727@gmail.com>
2025-11-22 19:31:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
-//your JS code here. If required.
-const main=document.querySelector(".container");
-for(let i=0;i<800;i++){
-	const square=document.createElement("div")
-	 square.classList.add("square");
+const main = document.querySelector(".container");
 
-	square.addEventListener("mouseover",()=>{
-		let color=randomColor();
-		square.style.backgroundColor=color;
-		
-		
-	})
-	 square.addEventListener("mouseout", () => {
-        square.style.backgroundColor = "rgb(29, 29, 29)";
-    });
+for (let i = 0; i < 800; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
 
-	
-	 main.appendChild(square);
-	
+  square.addEventListener("mouseover", () => {
+    square.style.backgroundColor = randomColor();
+  });
+
+  square.addEventListener("mouseout", () => {
+    square.style.backgroundColor = "rgb(29, 29, 29)";
+  });
+
+  main.appendChild(square);
 }
+
 function randomColor() {
-	const letters="0123456789ABCDEF";
-	let color="#";
-	for(let i=0;i<6;i++){
-		color+=letters[Math.floor(Math.random()*16)]
-	}
-	return color
-	
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
